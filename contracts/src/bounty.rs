3d1d436457484717158e807d377132f9d1cb4236
ilesoviy <ilisovij861@gmail.com>
2023-09-02 13:54:53 +0300

    Passed all tests

--- contracts/src/bounty.rs
+++ contracts/src/bounty.rs
@@ -38,15 +38,15 @@ pub fn bounty_create(
 ) -> u32 {
     // check args
     if name.len() == 0 {
-        // panic!("invalid name!");
+        // panic!("invalid name");
         return Error::InvalidName as u32
     }
     if reward_amount == 0 {
-        // panic!("zero reward isn't allowed!");
+        // panic!("zero reward isn't allowed");
         return Error::InvalidReward as u32
     }
     if deadline == 0 {
-        // panic!("invalid deadline!");
+        // panic!("invalid deadline");
         return Error::InvalidDeadline as u32
     }
     
@@ -88,23 +88,23 @@ pub fn bounty_fund(
     bounty_id: u32
 ) -> Error {
     if !fee_check(e) {
-        // panic!("fee hasn't been set yet!");
+        // panic!("fee hasn't been set yet");
         return Error::FeeNotSet
     }
 
     if !e.storage().instance().has(&DataKey::RegBounties(bounty_id)) {
-        // panic!("can't find bounty!");
+        // panic!("can't find bounty");
         return Error::BountyNotFound
     }
 
     let mut bounty: BountyInfo = bounty_load(e, bounty_id);
 
     if bounty.creator != *creator {
-        // panic!("creator and bounty mismatch!");
+        // panic!("creator and bounty mismatch");
         return Error::CreatorBountyMismatch
     }
     if bounty.status != BountyStatus::CREATED {
-        // panic!("invalid bounty status!");
+        // panic!("invalid bounty status");
         return Error::InvalidBountyStatus
     }
 
@@ -119,11 +119,11 @@ pub fn bounty_fund(
     let pay_token_client = token::Client::new(e, &bounty.pay_token);
 
     if pay_token_client.balance(&creator) < transfer_amount {
-        // panic!("creator's balance insufficient!");
+        // panic!("creator's balance insufficient");
         return Error::InsuffCreatorBalance
     }
     if pay_token_client.allowance(&creator, &contract) < transfer_amount {
-        // panic!("creator's allowance insufficient!");
+        // panic!("creator's allowance insufficient");
         return Error::InsuffCreatorAllowance
     }
 
@@ -148,22 +148,18 @@ pub fn bounty_submit(
     work_repo: &String
 ) -> u32 {
     // check args
-    if bounty_id == 0 {
-        // panic!("invalid bounty id!");
-        return Error::InvalidBountyID as u32
-    }
     if work_repo.len() == 0 {
-        // panic!("invalid repo!");
+        // panic!("invalid repo");
         return Error::InvalidWorkRepo as u32
     }
     
     if !participance_get(e, participant, bounty_id) {
-        // panic!("not participated!");
+        // panic!("not participated");
         return Error::NotParticipated as u32
     }
 
     if !bounty_check(e, bounty_id) {
-        // panic!("bounty not found!");
+        // panic!("bounty not found");
         return Error::BountyNotFound as u32
     }
 
@@ -204,6 +200,10 @@ pub fn bounty_approve(e: &Env,
     }
     let mut bounty: BountyInfo = bounty_load(e, work.bounty_id);
 
+    if bounty.creator != *creator {
+        // panic!("creator and bounty mismatch");
+        return Error::CreatorBountyMismatch;
+    }
     if bounty.status != BountyStatus::FUNDED {
         // panic!("invalid bounty status");
         return Error::InvalidBountyStatus
@@ -279,17 +279,17 @@ pub fn bounty_cancel(e: &Env,
     bounty_id: u32
 ) -> Error {
     if !e.storage().instance().has(&DataKey::RegBounties(bounty_id)) {
-        // panic!("can't find bounty!");
+        // panic!("can't find bounty");
         return Error::BountyNotFound;
     }
     let mut bounty = bounty_load(e, bounty_id);
 
     if bounty.creator != *creator {
-        // panic!("creator and bounty mismatch!");
+        // panic!("creator and bounty mismatch");
         return Error::CreatorBountyMismatch
     }
     if bounty.status != BountyStatus::FUNDED {
-        // panic!("bounty not available!");
+        // panic!("invalid bounty status");
         return Error::InvalidBountyStatus
     }
 
@@ -318,17 +318,17 @@ pub fn bounty_close(e: &Env,
     bounty_id: u32
 ) -> Error {
     if !e.storage().instance().has(&DataKey::RegBounties(bounty_id)) {
-        // panic!("can't find bounty!");
+        // panic!("can't find bounty");
         return Error::BountyNotFound
     }
     let mut bounty: BountyInfo = bounty_load(e, bounty_id);
 
     if bounty.status != BountyStatus::FUNDED {
-        // panic!("bounty not available!");
+        // panic!("bounty not available");
         return Error::InvalidBountyStatus
     }
     if bounty.end_date > e.ledger().timestamp() {
-        // panic!("bounty not timeout!");
+        // panic!("bounty not timeout");
         return Error::NoTimeout
     }
 
--- contracts/src/test.rs
+++ contracts/src/test.rs
@@ -25,9 +25,9 @@ fn create_bounty_contract<'a>(
 
 fn create_token_contract<'a>(
     e: &Env,
-    admin: &Address
+    issuer: &Address
 ) -> (Address, token::Client<'a>, token::AdminClient<'a>) {
-    let addr = e.register_stellar_asset_contract(admin.clone());
+    let addr = e.register_stellar_asset_contract(issuer.clone());
     (
         addr.clone(),
         token::Client::new(e, &addr),
@@ -35,20 +35,399 @@ fn create_token_contract<'a>(
     )
 }
 
-// timelock(cancel) test
+// approve test
 #[test]
-fn test4() {
+fn test1_approve() {
     let e = Env::default();
     e.mock_all_auths();
 
-    let token_admin: Address = Address::random(&e);
+    let token_issuer: Address = Address::random(&e);
+    let creator: Address = Address::random(&e);
+    let worker: Address = Address::random(&e);
+    
+    // create contract
+    let bounty_contract = create_bounty_contract(&e);
+
+    // create pay token
+    let pay_token = create_token_contract(&e, &token_issuer);
+    let pay_token_id: Address = pay_token.0;
+    let pay_token_client = pay_token.1;
+    let pay_token_admin_client = pay_token.2;
+    pay_token_admin_client.mint(&creator.clone(), &(10000 * MUL_VAL as i128));
+    pay_token_client.approve(&creator.clone(), &bounty_contract.address.clone(), &(10000 * MUL_VAL as i128), &INSTANCE_BUMP_AMOUNT);
+    
+    // init fee
+    let fee_rate = DEF_FEE_RATE;
+    let fee_wallet = Address::random(&e);
+    bounty_contract.set_fee(&fee_rate, &fee_wallet);
+
+    let old_timestamp = e.ledger().timestamp();
+    let new_timestamp = old_timestamp + ONE_DAY * 5;
+
+    // create bounty
+    let bounty_id: u32 = bounty_contract.create_bounty(
+        &creator,
+        &String::from_slice(&e, "Test4"),
+        &(1000 * MUL_VAL),
+        &pay_token_id,
+        &new_timestamp
+    );
+    assert_eq!(bounty_id, 0);
+
+    // fund bounty
+    let ret1 = bounty_contract.fund_bounty(
+        &creator,
+        &bounty_id
+    );
+    assert_eq!(ret1, Error::Success);
+    // check transfer
+    assert_eq!(
+        e.auths(),
+        std::vec![(
+            creator.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    bounty_contract.address.clone(),
+                    Symbol::new(&e, "fund_bounty"),
+                    (
+                        creator.clone(),
+                        bounty_id
+                    ).into_val(&e)
+                )),
+                sub_invocations: std::vec![
+                    AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            pay_token_id.clone(),
+                            symbol_short!("transfer"),
+                            (
+                                creator.clone(),
+                                bounty_contract.address.clone(),
+                                (1000 * MUL_VAL) as i128,
+                            ).into_val(&e)
+                        )),
+                        sub_invocations: std::vec![]
+                    },
+                    AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            pay_token_id.clone(),
+                            symbol_short!("transfer"),
+                            (
+                                creator.clone(),
+                                fee_wallet.clone(),
+                                (1000 * MUL_VAL * (fee_rate as u64) / u64::pow(10, FEE_DECIMALS)) as i128,
+                            ).into_val(&e)
+                        )),
+                        sub_invocations: std::vec![],
+                    }
+                ]
+            }
+        )]
+    );
+
+    // apply to bounty
+    let ret2 = bounty_contract.participate_bounty(
+        &worker,
+        &bounty_id
+    );
+    assert_eq!(ret2, Error::Success);
+
+    // submit work
+    let work_id = bounty_contract.submit_work(
+        &worker,
+        &bounty_id,
+        &String::from_slice(&e, "https://github.com/test_acc/test_repo")
+    );
+    assert_eq!(work_id, 0);
+
+    // approve work
+    let ret4 = bounty_contract.approve_work(
+        &creator,
+        &work_id
+    );
+    assert_eq!(ret4, Error::Success);
+    assert_eq!(
+        e.auths(),
+        std::vec![(
+            creator.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    bounty_contract.address.clone(),
+                    Symbol::new(&e, "approve_work"),
+                    (
+                        creator.clone(),
+                        bounty_id
+                    ).into_val(&e)
+                )),
+                sub_invocations: std::vec![]
+            }
+        )]
+    );
+    // check balances
+    assert_eq!(pay_token_client.balance(&worker), 
+        (1000 * MUL_VAL) as i128);
+}
+
+// reject test
+#[test]
+fn test2_reject() {
+    let e = Env::default();
+    e.mock_all_auths();
+
+    let token_issuer: Address = Address::random(&e);
+    let creator: Address = Address::random(&e);
+    let worker: Address = Address::random(&e);
+    
+    // create contract
+    let bounty_contract = create_bounty_contract(&e);
+
+    // create pay token
+    let pay_token = create_token_contract(&e, &token_issuer);
+    let pay_token_id: Address = pay_token.0;
+    let pay_token_client = pay_token.1;
+    let pay_token_admin_client = pay_token.2;
+    pay_token_admin_client.mint(&creator.clone(), &(10000 * MUL_VAL as i128));
+    pay_token_client.approve(&creator.clone(), &bounty_contract.address.clone(), &(10000 * MUL_VAL as i128), &INSTANCE_BUMP_AMOUNT);
+    
+    // init fee
+    let fee_rate = DEF_FEE_RATE;
+    let fee_wallet = Address::random(&e);
+    bounty_contract.set_fee(&fee_rate, &fee_wallet);
+
+    let old_timestamp = e.ledger().timestamp();
+    let new_timestamp = old_timestamp + ONE_DAY * 5;
+
+    // create bounty
+    let bounty_id: u32 = bounty_contract.create_bounty(
+        &creator,
+        &String::from_slice(&e, "Test4"),
+        &(1000 * MUL_VAL),
+        &pay_token_id,
+        &new_timestamp
+    );
+    assert_eq!(bounty_id, 0);
+
+    // fund bounty
+    let ret1 = bounty_contract.fund_bounty(
+        &creator,
+        &bounty_id
+    );
+    assert_eq!(ret1, Error::Success);
+    // check transfer
+    assert_eq!(
+        e.auths(),
+        std::vec![(
+            creator.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    bounty_contract.address.clone(),
+                    Symbol::new(&e, "fund_bounty"),
+                    (
+                        creator.clone(),
+                        bounty_id
+                    ).into_val(&e)
+                )),
+                sub_invocations: std::vec![
+                    AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            pay_token_id.clone(),
+                            symbol_short!("transfer"),
+                            (
+                                creator.clone(),
+                                bounty_contract.address.clone(),
+                                (1000 * MUL_VAL) as i128,
+                            ).into_val(&e)
+                        )),
+                        sub_invocations: std::vec![]
+                    },
+                    AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            pay_token_id.clone(),
+                            symbol_short!("transfer"),
+                            (
+                                creator.clone(),
+                                fee_wallet.clone(),
+                                (1000 * MUL_VAL * (fee_rate as u64) / u64::pow(10, FEE_DECIMALS)) as i128,
+                            ).into_val(&e)
+                        )),
+                        sub_invocations: std::vec![],
+                    }
+                ]
+            }
+        )]
+    );
+
+    // apply to bounty
+    let ret2 = bounty_contract.participate_bounty(
+        &worker,
+        &bounty_id
+    );
+    assert_eq!(ret2, Error::Success);
+
+    // submit work
+    let work_id = bounty_contract.submit_work(
+        &worker,
+        &bounty_id,
+        &String::from_slice(&e, "https://github.com/test_acc/test_repo")
+    );
+    assert_eq!(work_id, 0);
+
+    // reject work
+    let ret4 = bounty_contract.reject_work(
+        &creator,
+        &work_id
+    );
+    assert_eq!(ret4, Error::Success);
+    assert_eq!(
+        e.auths(),
+        std::vec![(
+            creator.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    bounty_contract.address.clone(),
+                    Symbol::new(&e, "reject_work"),
+                    (
+                        creator.clone(),
+                        bounty_id
+                    ).into_val(&e)
+                )),
+                sub_invocations: std::vec![]
+            }
+        )]
+    );
+}
+
+// cancel test
+#[test]
+fn test3_cancel() {
+    let e = Env::default();
+    e.mock_all_auths();
+
+    let token_issuer: Address = Address::random(&e);
+    let admin: Address = Address::random(&e);
     let creator: Address = Address::random(&e);
     
     // create contract
     let bounty_contract = create_bounty_contract(&e);
 
     // create pay token
-    let pay_token = create_token_contract(&e, &token_admin);
+    let pay_token = create_token_contract(&e, &token_issuer);
+    let pay_token_id: Address = pay_token.0;
+    let pay_token_client = pay_token.1;
+    let pay_token_admin_client = pay_token.2;
+    pay_token_admin_client.mint(&creator.clone(), &(10000 * MUL_VAL as i128));
+    pay_token_client.approve(&creator.clone(), &bounty_contract.address.clone(), &(10000 * MUL_VAL as i128), &INSTANCE_BUMP_AMOUNT);
+    
+    // init fee
+    let fee_rate = DEF_FEE_RATE;
+    let fee_wallet = Address::random(&e);
+    bounty_contract.set_fee(&fee_rate, &fee_wallet);
+
+    let old_timestamp = e.ledger().timestamp();
+    let new_timestamp = old_timestamp + ONE_DAY * 5;
+
+    // create bounty
+    let bounty_id: u32 = bounty_contract.create_bounty(
+        &creator,
+        &String::from_slice(&e, "Test4"),
+        &(1000 * MUL_VAL),
+        &pay_token_id,
+        &new_timestamp
+    );
+    assert_eq!(bounty_id, 0);
+
+    // fund bounty
+    let ret1 = bounty_contract.fund_bounty(
+        &creator,
+        &bounty_id
+    );
+    assert_eq!(ret1, Error::Success);
+    // check transfer
+    assert_eq!(
+        e.auths(),
+        std::vec![(
+            creator.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    bounty_contract.address.clone(),
+                    Symbol::new(&e, "fund_bounty"),
+                    (
+                        creator.clone(),
+                        bounty_id
+                    ).into_val(&e)
+                )),
+                sub_invocations: std::vec![
+                    AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            pay_token_id.clone(),
+                            symbol_short!("transfer"),
+                            (
+                                creator.clone(),
+                                bounty_contract.address.clone(),
+                                (1000 * MUL_VAL) as i128,
+                            ).into_val(&e)
+                        )),
+                        sub_invocations: std::vec![]
+                    },
+                    AuthorizedInvocation {
+                        function: AuthorizedFunction::Contract((
+                            pay_token_id.clone(),
+                            symbol_short!("transfer"),
+                            (
+                                creator.clone(),
+                                fee_wallet.clone(),
+                                (1000 * MUL_VAL * (fee_rate as u64) / u64::pow(10, FEE_DECIMALS)) as i128,
+                            ).into_val(&e)
+                        )),
+                        sub_invocations: std::vec![],
+                    }
+                ]
+            }
+        )]
+    );
+
+    // cancel bounty
+    let ret2 = bounty_contract.cancel_bounty(
+        &creator,
+        &bounty_id
+    );
+    assert_eq!(ret2, Error::Success);
+    assert_eq!(
+        e.auths(),
+        std::vec![(
+            creator.clone(),
+            AuthorizedInvocation {
+                function: AuthorizedFunction::Contract((
+                    bounty_contract.address.clone(),
+                    Symbol::new(&e, "cancel_bounty"),
+                    (
+                        creator.clone(),
+                        bounty_id
+                    ).into_val(&e)
+                )),
+                sub_invocations: std::vec![]
+            }
+        )]
+    );
+    // check refund
+    assert_eq!(pay_token_client.balance(&creator), 
+        (10000 * MUL_VAL - 1000 * MUL_VAL * (fee_rate as u64) / u64::pow(10, FEE_DECIMALS)) as i128);
+}
+
+// close(timelock) test
+#[test]
+fn test4_close() {
+    let e = Env::default();
+    e.mock_all_auths();
+
+    let token_issuer: Address = Address::random(&e);
+    let admin: Address = Address::random(&e);
+    let creator: Address = Address::random(&e);
+    
+    // create contract
+    let bounty_contract = create_bounty_contract(&e);
+
+    // create pay token
+    let pay_token = create_token_contract(&e, &token_issuer);
     let pay_token_id: Address = pay_token.0;
     let pay_token_client = pay_token.1;
     let pay_token_admin_client = pay_token.2;
@@ -132,20 +511,20 @@ fn test4() {
 
     // close bounty
     let ret2 = bounty_contract.close_bounty(
-        &token_admin,
+        &admin,
         &bounty_id
     );
     assert_eq!(ret2, Error::Success);
     assert_eq!(
         e.auths(),
         std::vec![(
-            token_admin.clone(),
+            admin.clone(),
             AuthorizedInvocation {
                 function: AuthorizedFunction::Contract((
                     bounty_contract.address.clone(),
                     Symbol::new(&e, "close_bounty"),
                     (
-                        token_admin.clone(),
+                        admin.clone(),
                         bounty_id
                     ).into_val(&e)
                 )),
