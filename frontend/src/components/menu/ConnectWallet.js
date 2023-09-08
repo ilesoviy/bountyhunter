bc65c4fc29f32029801254493dbf91677d9ef653
ilesoviy <ilisovij861@gmail.com>
2023-09-08 12:34:09 +0300

    Restored WalletConnect button

--- frontend/src/components/menu/ConnectWallet.js
+++ frontend/src/components/menu/ConnectWallet.js
@@ -40,14 +40,14 @@ const ConnectWallet = () => {
 
   return (
     <div className='connect-wallet'>
-      {/* {isEmpty(walletAddress) ? (
-        <button className='btn-main2' onClick={handleConnect}>Connect Wallet</button>
-      ) : (
+      {/* isEmpty(walletAddress) ? ( */
+        <button className='btn-main2' /* onClick={handleConnect} */>Connect Wallet</button>
+      /* ) : (
         <div className="flex items-center btn-main !px-[20px] !py-[10px]">
           <img alt='' className='w-5 h-5 text-white mr-2' src={'/images/icons/wallet.png'} />
           <span className="text-[14px]" onClick={handleDisconnect}>{walletAddress.slice(0, 4) + "..." + walletAddress.slice(38)}</span>
         </div>
-      )} */}
+      ) */}
     </div>
   )
 }
