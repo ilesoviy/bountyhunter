2dbe112393c355a7192a240928477e711f2d190b
ilesoviy <ilisovij861@gmail.com>
2023-09-25 09:50:20 +0300

    Trying to implement Redux

---
+++ frontend_vite/src/context/ReduxContext/reducers/network.jsx
@@ -0,0 +1,19 @@
+// import { createSlice } from '@reduxjs/toolkit';
+
+// const initialState = {
+//     connect: null,
+// };
+
+// export const networkSlice = createSlice({
+//     name: 'info',
+//     initialState,
+//     reducers: {
+//         changeConnect: (state, action) => {
+//             state.connect = action.payload;
+//         }
+//     }
+// });
+
+// export const { changeConnect } = networkSlice.actions;
+
+// export default networkSlice.reducer;
